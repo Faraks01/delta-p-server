902f2af6c0308287c4304213b837d7983877d3a3
Faraks <faraks01@gmail.com>
2019-04-03 20:21:07 +0300

    remove /build from .gitignore

---
+++ build/custom.d.ts
@@ -0,0 +1,10 @@
+declare module "*.svg" 
+declare module "*.json" {
+    const value: any;
+    export default value;
+} 
+declare module "react-yandex-maps"
+declare module "axios"
+declare module "redux"
+declare module "react-redux"
+declare module "thunk"
---
+++ build/precache-manifest.f7c724d83d3e4c0d7ba4653a4879aabc.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5d44640b825be912891ba739b8fc39b",
+    "url": "/static/media/background-1.f5d44640.jpg"
+  },
+  {
+    "revision": "77ec21a85546ea8de825",
+    "url": "/static/css/main.fd5bee28.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "462d4a2b7eaa0626ad6fca5e5fda9ad3",
+    "url": "/static/media/envelope.462d4a2b.svg"
+  },
+  {
+    "revision": "b39376a4982c20a406f3",
+    "url": "/static/js/2.f7ca7687.chunk.js"
+  },
+  {
+    "revision": "8c6af627c9874ea638ecb2e85c0fa289",
+    "url": "/static/media/worldmap.8c6af627.svg"
+  },
+  {
+    "revision": "d71e109a67fda3c4b9a758bae7721a10",
+    "url": "/static/media/logout.d71e109a.svg"
+  },
+  {
+    "revision": "f044dca9d8dbee7ae9977dc1ddaf6f0c",
+    "url": "/static/media/trash-can.f044dca9.svg"
+  },
+  {
+    "revision": "b73c0124c7b53983b8a7c4cd18ec917c",
+    "url": "/static/media/loop.b73c0124.svg"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "ee48a53637307041499f9754892f2793",
+    "url": "/static/media/expand-button.ee48a536.svg"
+  },
+  {
+    "revision": "2773ad8f076734fda67cb8876cf88e65",
+    "url": "/static/media/magnifier.2773ad8f.svg"
+  },
+  {
+    "revision": "3e11119ec55466e68d939354302801fa",
+    "url": "/static/media/arrow-right.3e11119e.svg"
+  },
+  {
+    "revision": "47ec24b14d2a9da84137bf2a17522d4e",
+    "url": "/static/media/arrow-left.47ec24b1.svg"
+  },
+  {
+    "revision": "77ec21a85546ea8de825",
+    "url": "/static/js/main.d7f2e2d7.chunk.js"
+  },
+  {
+    "revision": "29142f0e262f9cfcb4949a233b11c5bb",
+    "url": "/static/media/background-2.29142f0e.jpg"
+  },
+  {
+    "revision": "3fd95769737607eb40a92c6845adde92",
+    "url": "/static/media/background-3.3fd95769.jpg"
+  },
+  {
+    "revision": "4def78c00e4d60588699d6e9ab19dac6",
+    "url": "/static/media/gazprom-logo.4def78c0.png"
+  },
+  {
+    "revision": "b4b47f64fc03b57a9dd350f29f9483dd",
+    "url": "/static/media/novatek-logo.b4b47f64.png"
+  },
+  {
+    "revision": "4abaf29a82ff94858dcf634b169d6d01",
+    "url": "/static/media/rosneft-logo.4abaf29a.png"
+  },
+  {
+    "revision": "80d4409d4cc8bb68e42bb87d52547776",
+    "url": "/static/media/switch-on.80d4409d.svg"
+  },
+  {
+    "revision": "a1faac6b22f31820e879f6f44acbc0c1",
+    "url": "/static/media/switch-off.a1faac6b.svg"
+  },
+  {
+    "revision": "65f5fed3039dce950c4811c9506c4ae3",
+    "url": "/static/media/stub-image.65f5fed3.svg"
+  },
+  {
+    "revision": "842a20c0754db5073faede4c4c80e3e7",
+    "url": "/static/media/background_fill.842a20c0.svg"
+  },
+  {
+    "revision": "e99d3dc683fe6e1158d597ac4af9b91c",
+    "url": "/static/media/gmail.e99d3dc6.svg"
+  },
+  {
+    "revision": "0b000652cc1576118f8822cb19a6bafd",
+    "url": "/static/media/linkedin.0b000652.svg"
+  },
+  {
+    "revision": "59e8fd3e601ae108addd16eec54415f3",
+    "url": "/static/media/youtube.59e8fd3e.svg"
+  },
+  {
+    "revision": "6dfc962317a3d811b32110b3fec061b7",
+    "url": "/static/media/clocks.6dfc9623.svg"
+  },
+  {
+    "revision": "48b25176a5302babe374842eea155fd6",
+    "url": "/static/media/smartphone.48b25176.svg"
+  },
+  {
+    "revision": "b39376a4982c20a406f3",
+    "url": "/static/css/2.6a3dd0f4.chunk.css"
+  },
+  {
+    "revision": "f44ee9b325ba419b8276ca5ad0239082",
+    "url": "/index.html"
+  }
+];
