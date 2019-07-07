46d66fef4214bf80c6c7a478f8cac26e2058a7af
Faraks <faraks01@gmail.com>
2019-07-07 16:50:09 +0300

    == delta-p 1.1

---
+++ build/precache-manifest.a28b981e0fd757f7919e733bd398c079.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2263c0ff73d9f3d19671b3b449cce84c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7fd7c716d5f28e7e7d7",
+    "url": "/static/css/main.9939f227.chunk.css"
+  },
+  {
+    "revision": "f2be9906a3e0a182ba23",
+    "url": "/static/js/2.8de16d43.chunk.js"
+  },
+  {
+    "revision": "a7fd7c716d5f28e7e7d7",
+    "url": "/static/js/main.e7612d60.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fdb750b0ae806c55ff272cd5f0627318",
+    "url": "/static/media/admin-with-cogwheels.fdb750b0.svg"
+  },
+  {
+    "revision": "22c85be81e062177df7204ee43bbb8dc",
+    "url": "/static/media/background.22c85be8.jpg"
+  },
+  {
+    "revision": "91f551a1ed6bf78427aa2d5ef0420925",
+    "url": "/static/media/burger-menu.91f551a1.svg"
+  },
+  {
+    "revision": "f3b6c54c5cc90485a549a1ae6699a9c4",
+    "url": "/static/media/chrome.f3b6c54c.svg"
+  },
+  {
+    "revision": "6dfc962317a3d811b32110b3fec061b7",
+    "url": "/static/media/clocks.6dfc9623.svg"
+  },
+  {
+    "revision": "462d4a2b7eaa0626ad6fca5e5fda9ad3",
+    "url": "/static/media/envelope.462d4a2b.svg"
+  },
+  {
+    "revision": "edf69d104c6d6f1bc83565927d0b8e10",
+    "url": "/static/media/firefox.edf69d10.svg"
+  },
+  {
+    "revision": "e99d3dc683fe6e1158d597ac4af9b91c",
+    "url": "/static/media/gmail.e99d3dc6.svg"
+  },
+  {
+    "revision": "3d3a97276f6c3b745e59abdbb95b8797",
+    "url": "/static/media/homepage-pic-1.3d3a9727.png"
+  },
+  {
+    "revision": "c68ef478acda70c7f758db73d450de5e",
+    "url": "/static/media/homepage-pic-2.c68ef478.png"
+  },
+  {
+    "revision": "0b000652cc1576118f8822cb19a6bafd",
+    "url": "/static/media/linkedin.0b000652.svg"
+  },
+  {
+    "revision": "71bf1b061ee404c24d95804dafcb6d19",
+    "url": "/static/media/logo.71bf1b06.png"
+  },
+  {
+    "revision": "d71e109a67fda3c4b9a758bae7721a10",
+    "url": "/static/media/logout.d71e109a.svg"
+  },
+  {
+    "revision": "b73c0124c7b53983b8a7c4cd18ec917c",
+    "url": "/static/media/loop.b73c0124.svg"
+  },
+  {
+    "revision": "2773ad8f076734fda67cb8876cf88e65",
+    "url": "/static/media/magnifier.2773ad8f.svg"
+  },
+  {
+    "revision": "b4b964d2ce0bbef03baea601b9bb6446",
+    "url": "/static/media/opera.b4b964d2.svg"
+  },
+  {
+    "revision": "237a7f3b2a5860f0fe098c80be9b0595",
+    "url": "/static/media/safari.237a7f3b.svg"
+  },
+  {
+    "revision": "48b25176a5302babe374842eea155fd6",
+    "url": "/static/media/smartphone.48b25176.svg"
+  },
+  {
+    "revision": "f044dca9d8dbee7ae9977dc1ddaf6f0c",
+    "url": "/static/media/trash-can.f044dca9.svg"
+  },
+  {
+    "revision": "440376f37360ee4cd581803a82f5de75",
+    "url": "/static/media/triangle.440376f3.svg"
+  },
+  {
+    "revision": "8c6af627c9874ea638ecb2e85c0fa289",
+    "url": "/static/media/worldmap.8c6af627.svg"
+  },
+  {
+    "revision": "59e8fd3e601ae108addd16eec54415f3",
+    "url": "/static/media/youtube.59e8fd3e.svg"
+  }
+]);
